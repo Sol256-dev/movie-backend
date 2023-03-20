400693aff8b5019c8a45efc1bcd470b474b724c8
Sol256-dev <sol.malinga84@gmail.com>
2023-03-20 11:38:08 +0300

    GET: endpoints added

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,15 +1,42 @@
 const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 router.get("/movies", async (req, res, next) => {
-  res.send({ message: "Ok api is working 🚀" });
+  try {
+    const movies = await prisma.mediaItem.findMany({
+      include: { MediaType: true, Origin: true, Genre: true },
+    });
+    res.json(movies);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/movies/:id", async (req, res, next) => {
-  res.send({ message: "AWESOME!!! 🚀" });
+  try {
+    const { id } = req.params;
+    const movie = await prisma.mediaItem.findUnique({
+      where: { mediaId: id },
+      include: { MediaType: true, Origin: true, Genre: true },
+    });
+    res.json(movie);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // router.post("/movies", async (req, res, next) => {
-//   res.send({ message: "Ok api is working 🚀" });
+//   try {
+//     const data = req.body;
+//     const movie = await prisma.mediaItem.create({
+//       data: data,
+//     });
+//     res.json(movie);
+//   } catch (error) {
+//     next(error);
+//   }
 // });
 
 // router.delete("/movies/:id", async (req, res, next) => {
